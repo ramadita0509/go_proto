692bd2950a65d6790f2d842479b9b279ab8b4539
Rama Dita <rama.dita@bukalapak.com>
2022-12-18 14:00:24 +0700

    Update gRPC

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/ramadita0509/go_proto
 
 go 1.19
+
+require github.com/golang/protobuf v1.5.2
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- sample/generator.go
+++ sample/generator.go
@@ -1,1 +1,127 @@
 package sample
+
+import (
+	"github.com/golang/protobuf/ptypes"
+	"github.com/ramadita0509/go_proto/pb"
+)
+
+func NewKeyboard() *pb.Keyboard {
+	keyboard := &pb.Keyboard{
+		Layout:  randomKeyboardLayout(),
+		Backlit: randomBool(),
+	}
+	return keyboard
+}
+
+// NewCPU return a new sample CPU
+func NewCPU() *pb.CPU {
+
+	brand := randomCPUBrand()
+	name := randomCPUName(brand)
+
+	numberCores := randomInt(2, 8)
+	numberThreads := randomInt(numberCores, 12)
+
+	minGHz := randomFloat64(2.0, 3.5)
+	maxGHz := randomFloat64(minGHz, 5.0)
+
+	cpu := &pb.CPU{
+		Brand:         brand,
+		Name:          name,
+		NumberCores:   uint32(numberCores),
+		NumberThreads: uint32(numberThreads),
+		MinGhz:        minGHz,
+		MaxGhx:        maxGHz,
+	}
+	return cpu
+}
+
+// NewGPU returns a new GPU
+func NewGPU() *pb.GPU {
+	brand := randomGPUBrand()
+	name := randomGPUName(brand)
+
+	minGHz := randomFloat64(1.0, 1.5)
+	maxGHz := randomFloat64(minGHz, 2.0)
+
+	memory := &pb.Memory{
+		Value: uint64(randomInt(2, 6)),
+		Unit:  pb.Memory_GIGABYTE,
+	}
+
+	gpu := &pb.GPU{
+		Brand:  brand,
+		Name:   name,
+		MinGhz: minGHz,
+		MaxGhz: maxGHz,
+		Memory: memory,
+	}
+
+	return gpu
+}
+
+// NewRAM return a new sample RAM
+func NewRAM() *pb.Memory {
+	ram := &pb.Memory{
+		Value: uint64(randomInt(4, 64)),
+		Unit:  pb.Memory_GIGABYTE,
+	}
+
+	return ram
+}
+
+func NewSSD() *pb.Storage {
+	ssd := *&pb.Storage{
+		Driver: pb.Storage_SSD,
+		Memory: &pb.Memory{
+			Value: uint64(randomInt(128, 1064)),
+			Unit:  pb.Memory_GIGABYTE,
+		},
+	}
+	return &ssd
+}
+
+func NewHDD() *pb.Storage {
+	hdd := *&pb.Storage{
+		Driver: pb.Storage_HDD,
+		Memory: &pb.Memory{
+			Value: uint64(randomInt(1, 6)),
+			Unit:  pb.Memory_TERABYTE,
+		},
+	}
+	return &hdd
+}
+
+func NewScreen() *pb.Screen {
+	screen := &pb.Screen{
+		SizeInch:   randomFloat32(13, 17),
+		Resolution: randomScreenResoultion(),
+		Panel:      randomScreenPanel(),
+		Multitouch: randomBool(),
+	}
+
+	return screen
+}
+
+func NewLaptop() *pb.Laptop {
+	brand := randomLaptopBrand()
+	name := randomLaptopName(brand)
+	laptop := &pb.Laptop{
+		Id:       randomID(),
+		Brand:    brand,
+		Name:     name,
+		Cpu:      NewCPU(),
+		Ram:      NewRAM(),
+		Gpus:     []*pb.GPU{NewGPU()},
+		Storages: []*pb.Storage{NewSSD(), NewHDD()},
+		Screen:   NewScreen(),
+		Keyboard: NewKeyboard(),
+		Weight: &pb.Laptop_WeightKg{
+			WeightKg: randomFloat64(1.0, 3.0),
+		},
+		PriceUsd:    randomFloat64(1500, 3000),
+		ReleaseYear: uint32(randomInt(2015, 2022)),
+		UpdatedAt:   ptypes.TimestampNow(),
+	}
+	return laptop
+}
